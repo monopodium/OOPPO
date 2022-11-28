65ecec4411493ed3e77c97ef9d2fb44adad4cf92
ptbxzrt <ptbxzrt@aliyun.com>
2022-11-28 16:59:46 +0800

    refresh

--- install_libmemcached.sh
+++ install_libmemcached.sh
@@ -1,12 +1,8 @@
 CRT_DIR=$(pwd)
 
-ASIO_DIR=$CRT_DIR"/third_party/asio-1.24.0"
-LIBMEMCACHED_DIR=$CRT_DIR"/third_party/libmemcached-1.0.18"
-GRPC_DIR=$CRT_DIR"/third_party/grpc"
+LIBMEMCACHED_DIR=$CRT_DIR"/oppo_project/src/libmemcached-1.0.18"
 
-ASIO_INSTALL_DIR=$CRT_DIR"/third_party_install/asio"
 LIBMEMCACHED_INSTALL_DIR=$CRT_DIR"/third_party_install/libmemcached"
-GRPC_INSTALL_DIR=$CRT_DIR"/third_party_install/grpc"
 
 cd $LIBMEMCACHED_DIR
 ./configure --prefix=$LIBMEMCACHED_INSTALL_DIR
--- install_third_party.sh
+++ install_third_party.sh
@@ -1,7 +1,7 @@
 CRT_DIR=$(pwd)
 
 ASIO_DIR=$CRT_DIR"/third_party/asio-1.24.0"
-LIBMEMCACHED_DIR=$CRT_DIR"/third_party/libmemcached-1.0.18"
+LIBMEMCACHED_DIR=$CRT_DIR"/oppo_project/src/libmemcached-1.0.18"
 GRPC_DIR=$CRT_DIR"/third_party/grpc"
 
 ASIO_INSTALL_DIR=$CRT_DIR"/third_party_install/asio"
